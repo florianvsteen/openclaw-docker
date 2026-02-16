642050e963408b6748a0b854ec4a96deebffb9d2
florianvsteen <florian@flownet.be>
2026-02-16 10:13:04 +0100

    Update start-openclaw.sh

--- start-openclaw.sh
+++ start-openclaw.sh
@@ -310,11 +310,11 @@ EOFNODE
 mkdir -p /root/openclaw/skills
 
 # Check if the folder exists in the working directory
-if [ ! -d "/root/openclaw/skills/stock-analysis" ]; then
-    echo "Stock-analysis not found. Installing..."
-    clawhub install stock-analysis
+if [ ! -d "/root/openclaw/skills/market-environment-analysis" ]; then
+    echo "market-environment-analysis not found. Installing..."
+    clawhub install market-environment-analysis
 else
-    echo "Stock-analysis folder already exists. Skipping install."
+    echo "market-environment-analysis folder already exists. Skipping install."
 fi
 
 # ============================================================
